681d52cef62cfb37da8ee88e9bbf23053cbfc264
Patrick <pmdino93@gmail.com>
2022-12-27 11:29:49 -0700

    select message

--- webdriver.py
+++ webdriver.py
@@ -3,10 +3,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import os
-from dotenv import load_dotenv
-load_dotenv('./.env')
-user = os.environ['USERNAME']
-passw = os.environ['PASSWORD']
+# from dotenv import load_dotenv
+# load_dotenv('./.env')
+# user = os.environ['USERNAME']
+# passw = os.environ['PASSWORD']
 chrome_options = Options()
 chrome_options.add_argument("user-data-dir=selenium")
 chrome_options.add_experimental_option("detach", True)
@@ -38,3 +38,4 @@ driver.maximize_window()
 
 driver.get('https://www.instagram.com/direct/inbox/')
 
+message = driver.find_element(By.CSS_SELECTOR, 'div._ab8w._ab94._ab97._ab9f._ab9k._ab9p._ab9_._aba8._abcm').click()
