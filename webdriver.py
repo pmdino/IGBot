510c81b1ba7b65f13cfc7df9ec1a7e78bda8f04d
Patrick <pmdino93@gmail.com>
2022-12-27 08:35:16 -0700

    save profile, no more login

--- webdriver.py
+++ webdriver.py
@@ -8,30 +8,33 @@ load_dotenv('./.env')
 user = os.environ['USERNAME']
 passw = os.environ['PASSWORD']
 chrome_options = Options()
+chrome_options.add_argument("user-data-dir=selenium")
 chrome_options.add_experimental_option("detach", True)
 import time
 
 driver = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_options)
 driver.maximize_window()
 
-driver.get('https://instagram.com/')
+# driver.get('https://instagram.com/')
 
-#login
-time.sleep(15)
-username=driver.find_element("css selector", "input[name='username']")
-password=driver.find_element("css selector", "input[name='password']")
-username.clear()
-password.clear()
-username.send_keys(user)
-password.send_keys(passw)
-login = driver.find_element("css selector", "button[type='submit']").click()
+# #login
+# time.sleep(15)
+# username=driver.find_element("css selector", "input[name='username']")
+# password=driver.find_element("css selector", "input[name='password']")
+# username.clear()
+# password.clear()
+# username.send_keys(user)
+# password.send_keys(passw)
+# login = driver.find_element("css selector", "button[type='submit']").click()
 
-#save your login info?
-time.sleep(10)
-notnow = driver.find_element(By.XPATH, "//button[contains(text(), 'Not Now')]").click()
-#turn on notif
-time.sleep(10)
-notnow2 = driver.find_element(By.XPATH, "//button[contains(text(), 'Not Now')]").click()
-time.sleep(10)
-messages = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[5]/div/a/div").click()
+# #save your login info?
+# time.sleep(10)
+# notnow = driver.find_element(By.XPATH, "//button[contains(text(), 'Not Now')]").click()
+# #turn on notif
+# time.sleep(10)
+# notnow2 = driver.find_element(By.XPATH, "//button[contains(text(), 'Not Now')]").click()
+# time.sleep(10)
+# messages = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[5]/div/a/div").click()
+
+driver.get('https://www.instagram.com/direct/inbox/')
 
