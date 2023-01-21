e42405d3dfcb045acc067fab6952640f75aed938
Patrick <pmdino93@gmail.com>
2023-01-21 12:11:34 -0700

    changes

--- messaging.py
+++ messaging.py
@@ -11,13 +11,13 @@ chrome_options = Options()
 chrome_options.add_argument("user-data-dir=selenium")
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=3024, 1964')
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36')
 
 users = {
 
 }
-prompt = 'What have you been up to?\nWatching old movies.\nDid you watch anything interesting?\n'
+prompt = 'What is your name?\nMy name is Pieck Finger\nWhat do you do for work?\nI am the  Cart Titan, I fight for Marley\nWhat is your height?\nI am 5 foot 1 inches\nWhat day were you born?\nI was born on August 5th\nWhat is your age?\nI am 21 years old\nWho are your friends?\nMy friends are Porco and the Panzer squad. Zeke is like an older brother to me\nWhat color is your hair\nMy hair is black\nWhy is the Cart Titan so cool?\nIt has high stamina and can stay in titan form for months at a time.\nAre you a boy or girl?\nI am a girl.\n'
 
 driver = webdriver.Chrome('drivers/chromedriver.exe', chrome_options=chrome_options)
 
@@ -33,10 +33,10 @@ while running:
         selectMessages = driver.find_element(By.CSS_SELECTOR, "a[href*='inbox']").click()
         time.sleep(5)
         driver.refresh()
-        WebDriverWait(driver, 100).until(EC.visibility_of_element_located((By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[1]/div/div/div/div/div[2]/div[5]/div/a/div/div[1]/div/div[2]")))
-        selectMessage = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/section/div/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div/a').click()
-        time.sleep(5)
-        messageContext = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/section/div/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div/div[2]/div[2]/div/div/span[1]/span').text
+        WebDriverWait(driver, 10000).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div._ab8l._ab8n._ab8w._ab94._ab99._ab9f._ab9m._ab9p._abcm")))
+        selectMessage = driver.find_element(By.CSS_SELECTOR, 'a.x1i10hfl.xjbqb8w.x6umtig.x1b1mbwd.xaqea5y.xav7gou.x9f619.x1ypdohk.xt0psk2.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x16tdsg8.x1hl2dhg.xggy1nq.x1a2a7pz._a6hd').click()
+        time.sleep(10)
+        messageContext = driver.find_element(By.CSS_SELECTOR, 'span._aacl._aaco._aacu._aacy._aad7').text
         if messageContext != 'Sent you a message':
             selectedTitles = driver.find_elements(By.CSS_SELECTOR, 'div._aacl._aacp._aacw._aacx._aada')
             username = selectedTitles[-1].text
@@ -46,17 +46,17 @@ while running:
             messageContents = messageList[-1].text
             if messageContents == '/reset':
                 users[username] = prompt
-                messageArea = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/section/div/div/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea')
+                messageArea = driver.find_element(By.CSS_SELECTOR, 'textarea.focus-visible')
                 messageArea.click()
                 messageArea.send_keys('Chat session reset')
                 messageArea.send_keys(Keys.ENTER)
             else:
                 response = ''
                 response, users[username] = ask(messageContents, users[username])
-                time.sleep(5)
+                time.sleep(8)
                 if response == '':
                     time.sleep(30)
-                messageArea = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div/section/div/div/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea')
+                messageArea = driver.find_element(By.CSS_SELECTOR, 'textarea.focus-visible')
                 messageArea.click()
                 messageArea.send_keys(response)
                 time.sleep(1)
